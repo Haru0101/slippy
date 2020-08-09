de1172d2a93ed40235c243b966e142a2123a907c
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-08-09 14:32:59 +0900

    add vue-cli TypeScript導入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
-@charset 'UTF-8';
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-// reset.css
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+
+<style scoped>
 html, body, div, span, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 abbr, address, cite, code,
@@ -95,3 +125,6 @@ hr {
 input, select {
     vertical-align:middle;
 }
+
+
+</style>
