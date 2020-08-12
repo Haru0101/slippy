0556710b27607c5864d78259e0e51282d0677169
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-08-12 16:06:24 +0900

    add コントローラに飛ばす先のファイルを指定

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    public function index() {
+        // viewファイルのtests/testに飛ぶ
+        return view('tests.test');
+    }
 }
