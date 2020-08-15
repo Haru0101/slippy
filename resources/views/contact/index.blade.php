4f295a8f55012d3a7d2f777553b6a9c2320961ab
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-08-15 19:31:26 +0900

    fix レイアウトの調整

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.app')
 
+<!-- // appにある@yield('content')と対応させる -->
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Index') }}</div>
 
-index
+                <div class="card-body">
+
+                    index
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
